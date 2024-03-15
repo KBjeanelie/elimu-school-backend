d2676aa4680b2b01892a3c6d2952ae2ae239d820
Jean-Elie <jeaneliekubembula@gmail.com>
2024-03-15 12:32:26 +0100

    MAJ: mise en place de la classe active pour les liens de navigation

--- manager_dashboard/static/manager_dashboard/js/template.js
+++ manager_dashboard/static/manager_dashboard/js/template.js
@@ -11,35 +11,33 @@
     //Active class can be hard coded directly in html file also as required
 
     function addActiveClass(element) {
-      if (current === "") {
-        //for root url
-        if (element.attr('href').indexOf("index.html") !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
-          }
-        }
+      var currentPath = window.location.pathname;
+      var urlName = element.attr('href');
+  
+      // Vérifier si l'URL actuelle correspond exactement à l'URL du lien
+      console.log(currentPath)
+      console.log(urlName)
+      if (currentPath === urlName) {
+          element.addClass('active');
+          element.parents('.nav-item').addClass('active');
+          // Si le lien est dans un sous-menu, activer le sous-menu parent
+          element.closest('.collapse').addClass('show');
       } else {
-        //for other url
-        if (element.attr('href').indexOf(current) !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
+          // Si l'URL du lien est un sous-chemin de l'URL actuelle
+          if (currentPath.startsWith(urlName)) {
+              element.addClass('active');
+              element.parents('.nav-item').addClass('active');
+              // Si le lien est dans un sous-menu, activer le sous-menu parent
+              element.closest('.collapse').addClass('show');
           }
-          if (element.parents('.submenu-item').length) {
-            element.addClass('active');
-          }
-        }
       }
-    }
-
-    var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
-    $('.nav li a', sidebar).each(function () {
+  }
+  
+  $('.nav li a').each(function() {
       var $this = $(this);
       addActiveClass($this);
-    })
+  });
+  
 
     $('.horizontal-menu .nav li a').each(function () {
       var $this = $(this);
